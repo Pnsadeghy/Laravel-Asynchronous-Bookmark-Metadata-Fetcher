d830f5a04c00a8e2492dd31ef8e7c84be57218c0
Pouria <pnsadeghy@gmail.com>
2025-03-12 08:25:14 +0330

    Feat: Added bookmark factory

---
+++ database/factories/BookmarkFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bookmark>
+ */
+class BookmarkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'url' => $this->faker->unique()->url(),
+            'title' => $this->faker->unique()->words(1, true),
+            'description' => $this->faker->unique()->words(2, true)
+        ];
+    }
+}
